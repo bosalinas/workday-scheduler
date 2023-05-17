dc67b377fab6f3535c7814cd76651510a10f2950
Freebird <bo.salinas12@gmail.com>
2023-05-17 11:35:41 -0500

    added interval to change color time & save btn

--- Assets/script.js
+++ Assets/script.js
@@ -1,78 +1,61 @@
-// Look into JS substring. Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// Look into JS substring
+
 $(function () {
 
   var currentTime = dayjs();
   $('#currentDay').text(currentTime.format('dddd, MMMM D, YYYY h:mm A'));
 
-  //TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  var currentBlockEL = $('.col-2')
-  var pastHours = [];
-  var futureHours = [];
+  setInterval(currentTime, 1000);
 
-  for (var i = 0; i < currentTime; i++) {
-    pastHours.push(i + ":00");
-  }
-  for (var i = currentTime; i < 24; i++) {
-    futureHours.push(i + ":" + ("00"));
-  }
-  console.log(pastHours);
-  console.log(futureHours);
-  var hours = [
-    //   //list every hour in work day and show it on page
+  //event when clicking save button
+  $('.saveBtn').on('click', function() {
+    var description = $(this).siblings('textarea').val();
+    var id = $(this).parent().attr('id');
 
-  ];
-  var saveBtn = $('#save');
-  saveBtn.click(function () {
-    saveSchedule();
-  });
-  var userDescription = $('<textarea>')
-  var userTime = $('')
-  var userSchedule = userDescription &&
-    function saveSchedule() {
-      localStorage.setItem("time", JSON.stringify(userSchedule));
+    localStorage.setItem(id, description);
+  })
 
-      //     }
+//change color depending on current time
+  function changeColorBlocks() {
+    var timeBlock = $('.time-block');
 
-      //   // TODO: Add code to apply the past, present, or future class to each time
-      //   // block by comparing the id to the current hour. HINTS: How can the id
-      //   // attribute of each time-block be used to conditionally add or remove the
-      //   // past, present, and future classes? How can Day.js be used to get the
-      //   // current hour in 24-hour time?
-      function changeColor() {
-        var pastColor = { 'color': #d3d3d3 };
-        var presentColor = { 'color': #ff6961 };
-        var futureColor = {
-          'color': #77dd77
-    if currentTime = //time of schedule block//
-          {
-            presentColor;
-          };
-          if currentTime >= //time of schedule block//
-          {
-            futureColor;
-          };
-          else {
-            pastColor;
-          };
-        }
+    for (var i = 0; i < timeBlock.length; i++) {
+  //recieved help with this and don't understand these var
+      var hour = dayjs().hour();
+      var blockId = $(timeBlock[i]).attr('id');
+      var blockHour = blockId.split("-")[1];
+
+//change color depending on current time if statements
+      if (blockHour < hour) {
+        $(timeBlock[i]).removeClass('present');
+        $(timeBlock[i]).removeClass('future');
+        $(timeBlock[i]).addClass('past');
+      } else if (blockHour == hour) {
+        $(timeBlock[i]).removeClass('future');
+        $(timeBlock[i]).removeClass('past');
+        $(timeBlock[i]).addClass('present');
+      } else {
+        $(timeBlock[i]).removeClass('past');
+        $(timeBlock[i]).removeClass('present');
+        $(timeBlock[i]).addClass('future');
       };
-      //
-      // TODO: Add code to get any user input that was saved in localStorage and set
-      // the values of the corresponding textarea elements. HINT: How can the id
-      // attribute of each time-block be used to do thi
-      function getData() {
-        var getUserDescription = localStorage.getItem("description");
-        var getUserTime = localStorage.getItem("userTime");
-        getUserTime.textContent = userTime
-        getUserDescription.textContent = userDescription
 
-        // TODO: Add code to display the current date in the header of the page.
-      };
     };
+  };
+
+//get from local storage and display on HTML
+  function getData() {
+    $("#hour-9 textarea").val(localStorage.getItem('hour-9'));
+    $("#hour-10 textarea").val(localStorage.getItem('hour-10'));
+    $("#hour-11 textarea").val(localStorage.getItem('hour-11'));
+    $("#hour-12 textarea").val(localStorage.getItem('hour-12'));
+    $("#hour-13 textarea").val(localStorage.getItem('hour-13'));
+    $("#hour-14 textarea").val(localStorage.getItem('hour-14'));
+    $("#hour-15 textarea").val(localStorage.getItem('hour-15'));
+    $("#hour-16 textarea").val(localStorage.getItem('hour-16'));
+    $("#hour-17 textarea").val(localStorage.getItem('hour-17'));
+  };
+
+  setInterval(changeColorBlocks, 30000);
+  getData();
+});   
